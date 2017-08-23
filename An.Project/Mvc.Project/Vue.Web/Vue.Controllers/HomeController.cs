842e91f5a1cf8efd36a06227f09a9850f03cae27
lvfuan@163.com <lvfuan@163.com>
2017-08-23 18:20:01 +0800

    init

--- An.Project/Mvc.Project/Vue.Web/Vue.Controllers/HomeController.cs
+++ An.Project/Mvc.Project/Vue.Web/Vue.Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace Vue.Controllers
     {
         public HomeController() { }
         public ActionResult Index()
-        {
+        { 
             return View();
         }
     }
